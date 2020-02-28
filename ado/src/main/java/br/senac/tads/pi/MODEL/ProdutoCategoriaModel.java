3184c4cb7a9408a225bb713642c824bb67d21ab9
Matheus Antonio <matheusantonio232@gmail.com>
2020-02-28 13:27:41 -0300

    Criação de models, primeira versão da view

---
+++ ado/src/main/java/br/senac/tads/pi/MODEL/ProdutoCategoriaModel.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi.MODEL;
+
+/**
+ *
+ * @author maria.imbeserra
+ */
+public class ProdutoCategoriaModel {
+
+    private int idProduto;
+    private String idCategoria;
+
+    public ProdutoCategoriaModel() {
+    }
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(String idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+}
