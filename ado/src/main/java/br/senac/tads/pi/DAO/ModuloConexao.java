f768b61078b6c09ce849c48a4b2de7a93265b712
Matheus Antonio <matheusantonio232@gmail.com>
2020-02-28 13:30:07 -0300

    Criação de models, primeira versão da view

---
+++ ado/src/main/java/br/senac/tads/pi/DAO/ModuloConexao.java
@@ -0,0 +1,67 @@
+package br.senac.tads.pi.DAO;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+/*
+ @see Conectar() - gera conexão ao banco de dados bd_loja_informatica
+ @see CloseConnection() - para a conexão e fecha os comandos de suporte ao sql...
+
+ */
+
+public class ModuloConexao {
+
+    private static String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static String LOGIN = "root";
+    private static String SENHA = "";
+    private static String URL = "jdbc:mysql://localhost:3306/PRODUTODB?useTimezone=true&serverTimezone=UTC&useSSL=false";
+
+    public static Connection Conectar() {
+        java.sql.Connection conexao = null;
+        try {
+            Class.forName(DRIVER);
+            conexao = DriverManager.getConnection(URL, LOGIN, SENHA);
+            return conexao;
+        } catch (Exception ErrorSQL) {
+            JOptionPane.showMessageDialog(null, "Não foi possivel conectar ao Banco de Dados" + ErrorSQL);
+            return null;
+        }
+
+    }
+
+    public static void CloseConnection(Connection conn) {
+
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (Exception ErrorSQL) {
+            JOptionPane.showMessageDialog(null, "Não foi possivel conectar ao Banco de Dados" + ErrorSQL);
+        }
+
+    }
+
+    public static void CloseConnection(Connection conn, PreparedStatement ST) {
+        CloseConnection(conn);
+        try {
+            if (ST != null) {
+                ST.close();
+            }
+        } catch (SQLException ErrorSQL) {
+            throw new RuntimeException("Erro! Não foi possível fechar o PreparedStatement: " + ErrorSQL);
+        }
+
+    }
+
+    public static void CloseConnection(Connection conn, PreparedStatement ST, ResultSet RS) {
+        CloseConnection(conn, ST);
+        try {
+            if (RS != null) {
+                RS.close();
+            }
+        } catch (SQLException ErrorSQL) {
+            throw new RuntimeException("Erro! Não foi possível fechar o ResustSet: " + ErrorSQL);
+        }
+
+    }
+
+}
---
+++ ado/src/main/java/br/senac/tads/pi/MODEL/ProdutoModel.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi.MODEL;
+
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author
+ */
+public class ProdutoModel {
+
+    private int id;
+    private String nome;
+    private String desc;
+    private double precoCompra;
+    private double precoVenda;
+    private int qtde;
+    private boolean disponibilidade;
+    private Timestamp dataCadastro;
+
+    public ProdutoModel() {
+    }
+
+    public ProdutoModel(String nome, String desc, double precoCompra, double precoVenda, int qtde, boolean disponibilidade, Timestamp dataCadastro) {
+
+        this.nome = nome;
+        this.desc = desc;
+        this.precoCompra = precoCompra;
+        this.precoVenda = precoVenda;
+        this.qtde = qtde;
+        this.disponibilidade = disponibilidade;
+        this.dataCadastro = dataCadastro;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public double getPrecoCompra() {
+        return precoCompra;
+    }
+
+    public void setPrecoCompra(double precoCompra) {
+        this.precoCompra = precoCompra;
+    }
+
+    public double getPrecoVenda() {
+        return precoVenda;
+    }
+
+    public void setPrecoVenda(double precoVenda) {
+        this.precoVenda = precoVenda;
+    }
+
+    public int getQtde() {
+        return qtde;
+    }
+
+    public void setQtde(int qtde) {
+        this.qtde = qtde;
+    }
+
+    public boolean isDisp() {
+        return disponibilidade;
+    }
+
+    public void setDisp(boolean disp) {
+        this.disponibilidade = disp;
+    }
+
+    public Timestamp getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(Timestamp dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+}
