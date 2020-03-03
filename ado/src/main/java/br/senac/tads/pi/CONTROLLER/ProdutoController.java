c1110dc3982978b2b91a90c16d21aa4cb3322379
RaphaelaWagner <ellaw@uol.com.br>
2020-03-03 13:43:43 -0300

    Controller incompleto

---
+++ ado/src/main/java/br/senac/tads/pi/CONTROLLER/ProdutoController.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi.CONTROLLER;
+
+import java.util.ArrayList;
+import br.senac.tads.pi.MODEL.*;
+import br.senac.tads.pi.DAO.*;
+
+/**
+ *
+ * @author Cruiser
+ */
+public class ProdutoController {
+
+}
